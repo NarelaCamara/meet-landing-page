dbd0b0a3beb0cd47e422c83ceb42673bcc7c4080
RoxanaCamara <roxana.camara07@gmail.com>
2025-07-16 14:20:15 -0300

    feat: desing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,68 @@
 import "./App.css";
 import footer from "./assets/mobile/image-footer.jpg";
+import hero from "./assets/tablet/image-hero.png";
+import logo from "./assets/logo.svg";
 
 function App() {
   return (
     <>
-      <p>
-        Meet makes it easy to connect with others face-to-face virtually and
-        collaborate across any device.
-      </p>
-      <img src={footer} className="logo" alt="Vite logo" />
+      <div className="p-8 flex flex-col items-center ">
+        <img src={logo} className="h-[28px] m-12" alt="Meet logo" />
+
+        <img src={hero} className="w-full" alt="Meet hero" />
+
+        <h1 className="font-[Red Hat Display] text-[2.5rem] font-bold text-center w-[15rem]">
+          Group Chat for Everyone
+        </h1>
+
+        <p className="font-[Red Hat Display]  text-[1.125]  text-[#87879D] text-center py-6">
+          Meet makes it easy to connect with others face-to-face virtually and
+          collaborate across any device.
+        </p>
+
+        <button className="bg-[#4D96A9]  p-4 px-10 text-white font-medium text-[1rem] rounded-[1.825rem] text-center ">
+          Download <span>v1.3</span>
+        </button>
+        <button className="bg-[#855FB1]  p-4 px-10 text-white font-medium text-[1rem] rounded-[1.825rem] text-center mt-4">
+          What is it?
+        </button>
+
+        <div className="grid grid-cols-2 grid-rows-2 gap-6 my-16 ">
+          <img src={footer} className="w-full rounded-lg" alt="Vite " />
+          <img src={footer} className="w-full rounded-lg" alt="Vite " />
+          <img src={footer} className="w-full rounded-lg" alt="Vite " />
+          <img src={footer} className="w-full rounded-lg" alt="Vite " />
+        </div>
+
+        <h3 className="font-[Red Hat Display] text-[#4D96A9] text-[0.75rem] font-bold text-center uppercase tracking-[4px]">
+          Built for modern use
+        </h3>
+        <h2 className="font-[Red Hat Display] text-[2.5rem] font-bold text-center">
+          Smarter meetings, all in one place
+        </h2>
+        <p className="font-[Red Hat Display]  text-[1.125]  text-[#87879D] text-center">
+          Send messages, share files, show your screen, and record your meetings
+          — all in one workspace. Control who can join with invite-only team
+          access, data encryption, and data export.
+        </p>
+      </div>
+
+      <div className=" bg-[url(./assets/mobile/image-footer.jpg)] bg-cover from-[#4D96A9] bg-blend-overlay">
+        <div className="p-8 flex flex-col items-center">
+          <h1 className="font-[Red Hat Display] text-[2.5rem] text-white font-bold text-center w-[15rem]">
+            Experience more together
+          </h1>
+
+          <p className="font-[Red Hat Display]  text-[1.125]  text-white text-center py-6">
+            Stay connected with reliable HD meetings and unlimited one-on-one
+            and group video sessions.
+          </p>
+
+          <button className="bg-[#855FB1]  p-4 px-10 text-white font-medium text-[1rem] rounded-[1.825rem] text-center ">
+            Download <span>v1.3</span>
+          </button>
+        </div>
+      </div>
     </>
   );
 }
