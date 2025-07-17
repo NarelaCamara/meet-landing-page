60bc8e9ed739361394197c8b8b0d3d0ac93bc085
RoxanaCamara <roxana.camara07@gmail.com>
2025-07-17 00:58:19 -0300

    feat: grid

--- src/App.tsx
+++ src/App.tsx
@@ -20,14 +20,16 @@ function App() {
           collaborate across any device.
         </p>
 
-        <button className="bg-[#4D96A9]  p-4 px-10 text-white font-bold text-[1rem] rounded-[1.825rem] text-center ">
-          Download <span className=" text-[#8FE3F9] font-bold">v1.3</span>
-        </button>
-        <button className="bg-[#855FB1]  p-4 px-10 text-white font-bold text-[1rem] rounded-[1.825rem] text-center mt-4">
-          What is it?
-        </button>
+        <div className="grid sm:grid-cols-2 gap-6 md:grid-rows-2  mt-4">
+          <button className="bg-[#4D96A9]  p-4 px-10 text-white font-bold text-[1rem] rounded-[1.825rem] text-center ">
+            Download <span className=" text-[#8FE3F9] font-bold">v1.3</span>
+          </button>
+          <button className="bg-[#855FB1]  p-4 px-10 text-white font-bold text-[1rem] rounded-[1.825rem] text-center">
+            What is it?
+          </button>
+        </div>
 
-        <div className="flex flex-col items-center mt-8">
+        <div className="flex flex-col items-center mt-8 ">
           <div className="inline-block h-[80px] w-0.5 bg-[#D1D1DF]"></div>
           <div className="w-[56px] h-[56px] bg-white border-[#D1D1DF] border-2 rounded-full text-[#87879D] text-[1rem]  font-bold font-[Red Hat Display] flex items-center justify-center ">
             02
@@ -54,28 +56,30 @@ function App() {
       </div>
 
       <div className="pt-16">
-        <div className=" bg-[url(./assets/mobile/image-footer.jpg)] bg-cover from-[#4D96A9] mt-8   bg-[#4D96A9] bg-blend-soft-light">
-          <div className="relative top-[-7rem] mt-8">
-            <div className="flex flex-col items-center ">
-              <div className="inline-block h-[80px] w-0.5 bg-[#D1D1DF]"></div>
-              <div className="w-[56px] h-[56px] bg-white border-[#D1D1DF] border-2 rounded-full text-[#87879D] text-[1rem]  font-bold font-[Red Hat Display] flex items-center justify-center ">
-                02
+        <div className=" bg-[url(./assets/mobile/image-footer.jpg)] md:bg-[url(./assets/desktop/image-footer.jpg)] bg-contain from-[#4D96A9] mt-8   bg-[#4D96A9] bg-blend-soft-light">
+          <div className="pb-16">
+            <div className="relative top-[-7rem] mt-8">
+              <div className="flex flex-col items-center ">
+                <div className="inline-block h-[80px] w-0.5 bg-[#D1D1DF]"></div>
+                <div className="w-[56px] h-[56px] bg-white border-[#D1D1DF] border-2 rounded-full text-[#87879D] text-[1rem]  font-bold font-[Red Hat Display] flex items-center justify-center ">
+                  02
+                </div>
               </div>
             </div>
-          </div>
-          <div className="p-8 flex flex-col items-center">
-            <h1 className="font-[Red Hat Display] text-[2.5rem] text-white font-bold text-center w-[15rem]">
-              Experience more together
-            </h1>
+            <div className="p-8 flex flex-col items-center">
+              <h1 className="font-[Red Hat Display] text-[2.5rem] text-white font-bold text-center w-[15rem]">
+                Experience more together
+              </h1>
 
-            <p className="font-[Red Hat Display]  text-[1.125]  text-white text-center py-6">
-              Stay connected with reliable HD meetings and unlimited one-on-one
-              and group video sessions.
-            </p>
+              <p className="font-[Red Hat Display]  text-[1.125]  text-white text-center py-6">
+                Stay connected with reliable HD meetings and unlimited
+                one-on-one and group video sessions.
+              </p>
 
-            <button className="bg-[#855FB1]  p-4 px-10 text-white font-bold text-[1rem] rounded-[1.825rem] text-center ">
-              Download <span className=" text-[#D9B8FF] font-bold">v1.3</span>
-            </button>
+              <button className="bg-[#855FB1]  p-4 px-10 text-white font-bold text-[1rem] rounded-[1.825rem] text-center ">
+                Download <span className=" text-[#D9B8FF] font-bold">v1.3</span>
+              </button>
+            </div>
           </div>
         </div>
       </div>
